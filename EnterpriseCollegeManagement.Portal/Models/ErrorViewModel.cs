31073179736de7d9ad8dca0924ed5a92e0209b4f
shajanjjacob <shajanjjacob369@gmail.com>
2026-07-31 22:40:16 +0530

    Initial solution structure

---
+++ EnterpriseCollegeManagement.Portal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EnterpriseCollegeManagement.Portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
